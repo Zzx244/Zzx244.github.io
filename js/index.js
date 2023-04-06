5b03b7e204a27e0fd6ff0a562cf03e3903012107
Zzx244 <1095083737@qq.com>
2023-04-06 19:33:00 +0800

    init

---
+++ js/index.js
@@ -0,0 +1,48 @@
+function over1() {undefined
+      var obj1 = document.getElementById("main_i1");
+      var obj2 = document.getElementById("main_i2");
+      var obj3 = document.getElementById("main_i3");
+      obj1.style.cssText = "display:block";
+      obj2.style.cssText = "display:none;width:0px;";
+      obj3.style.cssText = "display:none;width:0px;";
+}
+function over2() {undefined
+      var obj1 = document.getElementById("main_i1");
+      var obj2 = document.getElementById("main_i2");
+      var obj3 = document.getElementById("main_i3");
+      obj1.style.cssText = "display:none;width:0px;";
+      obj2.style.cssText = "display:block";
+      obj3.style.cssText = "display:none;width:0px;";
+}
+function over3() {undefined
+      var obj1 = document.getElementById("main_i1");
+      var obj2 = document.getElementById("main_i2");
+      var obj3 = document.getElementById("main_i3");
+      obj1.style.cssText = "display:none;width:0px;";
+      obj2.style.cssText = "display:none;width:0px;";
+      obj3.style.cssText = "display:block;";
+}
+function out1() {undefined
+      var obj1 = document.getElementById("main_i1");
+      var obj2 = document.getElementById("main_i2");
+      var obj3 = document.getElementById("main_i3");
+      obj1.style.cssText = "display:block";
+      obj2.style.cssText = "display:block";
+      obj3.style.cssText = "display:block";
+}
+function out2() {undefined
+      var obj1 = document.getElementById("main_i1");
+      var obj2 = document.getElementById("main_i2");
+      var obj3 = document.getElementById("main_i3");
+      obj1.style.cssText = "display:block";
+      obj2.style.cssText = "display:block";
+      obj3.style.cssText = "display:block";
+}
+function out3() {undefined
+      var obj1 = document.getElementById("main_i1");
+      var obj2 = document.getElementById("main_i2");
+      var obj3 = document.getElementById("main_i3");
+      obj1.style.cssText = "display:block";
+      obj2.style.cssText = "display:block";
+      obj3.style.cssText = "display:block";
+}
